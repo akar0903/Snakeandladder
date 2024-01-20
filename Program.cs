f7130917d566a6c687a36c2df628933791e8f205
akar0903 <av0019@srmist.edu.in>
2024-01-20 11:21:25 +0530

    Use Case 2: Dice rolling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@ class Program
     static void Main()
     {
         int playerposition = 0;
+        int diceroll = DiceRoll();
+        Console.WriteLine(diceroll);
         Console.ReadLine();
+    }
+    static int DiceRoll()
+    {
+        Random random = new Random();
+        return random.Next(1, 7);
+
     }
     
 }
